400d859efd986672adb7099eb741df4957ecd89b
UDUKUNDA <udukundaelysee@gmail.com>
2023-08-14 10:51:23 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{}".format(row[i]))
+            if i == 0:
+                print("{:d}".format(row[i]), end="")
             else:
-                print("{} ".format(row[i]), end="")
+                print(" {:d}".format(row[i]), end="")
+        print()
