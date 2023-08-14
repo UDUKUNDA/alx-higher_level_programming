128be2d39637458d8d3ca8ae441f06058130d5bb
UDUKUNDA <udukundaelysee@gmail.com>
2023-08-14 09:16:01 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    empstr = ""
-    for j in my_string:
-        if j != 'c' and j != 'C':
-            new_string += j
-            return new_string
+    y=[i for i in my_string if i != 'c' and i != 'C']
+    return ("".join(y))
