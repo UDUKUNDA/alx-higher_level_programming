f0f9bee0745112bedf2ffa191a0b31b0fd7d00c8
UDUKUNDA <udukundaelysee@gmail.com>
2023-08-14 02:04:58 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,6 @@
 
 def element_at(my_list, idx):
     """element_at is going to retrieve element at index"""
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx >= (len(my_list)):
         return None
     return (my_list[idx])
