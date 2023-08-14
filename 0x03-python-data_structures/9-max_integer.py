88380c14d482e33ae057b026ab28ed9f39000b96
UDUKUNDA <udukundaelysee@gmail.com>
2023-08-14 15:50:15 +0000

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list = "":
+        return None
+    big = my_list[0]
+    for i in range(my_list):
+        if my_list[i] > big:
+            big = my_list[i]
+    return big
