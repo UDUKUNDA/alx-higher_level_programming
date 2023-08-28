0d406baba39d58a4f07b9b73cdc65d93e848631c
UDUKUNDA <udukudndaelysee@gmail.com>
2023-08-28 20:10:44 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+
     length = 0
-    try:
-        for a in range(x):
-            print("{}".format(my_list[i]), end ="")
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
             length += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
     print("")
     return (length)
-
