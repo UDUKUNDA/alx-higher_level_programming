49e92b86cb6a66ffa81426f1cb5b3979f82ba7da
UDUKUNDA <udukudndaelysee@gmail.com>
2023-08-28 20:05:01 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
     length = 0
     try:
         for a in range(x):
-            print(my_list[i], end =' ')
+            print("{}".format(my_list[i]), end ="")
             length += 1
     except IndexError:
-        print("\nTrying to access invalid index")
-    print()
-    return length
+        break
+    print("")
+    return (length)
 
