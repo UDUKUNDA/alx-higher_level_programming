5d5b6ff619c06c3ca3a19347363a64cc6199e27a
UDUKUNDA <udukudndaelysee@gmail.com>
2023-08-30 02:40:06 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defined a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """We will Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
