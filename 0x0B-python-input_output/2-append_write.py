acd948474f3663c1a1870a9ea73e98d90ef741c1
Elysee UDUKUNDA <125474615+UDUKUNDA@users.noreply.github.com>
2023-10-31 01:10:26 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines a file-appending function."""
-
-
-def append_write(filename="", text=""):
-    """Appends a string to the end of a file
-    """
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
--- 0x0B-python-input_output/README.md
+++
@@ -1,1 +0,0 @@
-This project is going to be all about input output in python
