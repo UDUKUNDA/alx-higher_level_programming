bd6442d4f1412bcd23fc1c849d734c5e09052e1f
UDUKUNDA <udukudndaelysee@gmail.com>
2024-02-18 03:43:42 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""This one will fetch from a url that is specified below"""
-
+"""fetches https://intranet.hbtn.io/status"""
 import urllib.request
+
 
 if __name__ == "__main__":
     """fetches https://intranet.hbtn.io/status"""
-
-    myUrl = "https://alx-intranet.hbtn.io/status"
-
-    with urllib.request.urlopen(myUrl) as response:
-
-        bdy = response.read()
-        bdy_str = bdy.decode('utf-8')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        body_str = body.decode('utf-8')
 
     print("Body response:")
-    print("\t- type: {}".format(type(bdy)))
-    print("\t- content: {}".format(bdy))
-    print("\t- utf-8 content: {}".format(bdy_str))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body_str))
 
